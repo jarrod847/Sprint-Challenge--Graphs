28eec429701d3e94777978d74faad41138220e4e
jarrodskahill <jarrodskahill@gmail.com>
2020-06-12 14:06:04 -0700

    MVP

--- adv.py
+++ adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph = literal_eval(open(map_file, "r").read())
@@ -31,17 +31,36 @@ traversal_path = []
 
 
 def traversal():
+    visited = {}
+    path = []
+    visited[player.current_room.id] = player.current_room.get_exits()
+    while len(visited) < len(room_graph):
+        room_id = player.current_room.id
+        room_dir = player.current_room.get_exits()
+        if room_id not in visited:
+            visited[room_id] = room_dir
+            previous = path[-1]
+            visited[room_id].remove(previous)
 
-    visited = set()
+        if len(visited[room_id]) == 0:
+            previous = path[-1]
+            path.pop()
+            traversal_path.append(previous)
+            player.travel(previous)
 
-    paths = []
-
-    while len(visited) < room_graph:
-        for i in player.current_room.get_exits():
-            visited.add(player.travel(i))
-
-    print(player.current_room.id)
-    print(player.current_room.get_exits())
+        else:
+            direction = random.choice(visited[room_id])
+            visited[room_id].pop(visited[room_id].index(direction))
+            traversal_path.append(direction)
+            if direction == 'n':
+                path.append('s')
+            elif direction == 's':
+                path.append('n')
+            elif direction == 'e':
+                path.append('w')
+            elif direction == 'w':
+                path.append('e')
+            player.travel(direction)
 
 
 traversal()
@@ -67,12 +86,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
